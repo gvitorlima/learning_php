b9d89a288ccad5f5e83adf225133dca2604ca95e
Gabriel Vitor <git624886@gmail.com>
2023-02-07 10:32:03 -0300

    format: remoção de obj

--- public/index.php
+++ public/index.php
@@ -7,7 +7,3 @@ use App\Http\Router;
 require_once __DIR__ . '/../vendor/autoload.php';
 include_once __DIR__ . '/../config/init.php';
 
-$objResponse = new Response(200, 'yaa');
-$objRequest = new Request();
-
-$objRouter = new Router(URL);
