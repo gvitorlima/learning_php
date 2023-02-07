c78630815c8a834cb2ffbc2fa3f542ef3c5c4e40
Gabriel Vitor <git624886@gmail.com>
2023-02-07 10:32:37 -0300

    feat: obj de classes e include das rotas
    classes de request, response e router

--- config/init.php
+++ config/init.php
@@ -1,4 +1,21 @@
 <?php
 
+use App\Http\Request;
+use App\Http\Response;
+use App\Http\Router;
+
 define('URL', '127.0.0.1:8080/prefix');
 
+$routesPath = __DIR__ . '/../routes';
+$routesPath = glob($routesPath . '/*/*.php');
+
+foreach ($routesPath as $route) {
+  include_once($route);
+}
+
+$objResponse = new Response(200, 'yaa');
+$objRequest = new Request();
+
+$objRouter = new Router(URL);
+
+$objRouter->getRoute();
