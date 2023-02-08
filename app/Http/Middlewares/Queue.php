725ab90a2cd9a12e9205771b62415609df4f4047
Gabriel Vitor <git624886@gmail.com>
2023-02-08 11:44:14 -0300

    feat: queue

---
+++ app/Http/Middlewares/Queue.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Middlewares;
+
+use App\Http\Request;
+use Closure;
+use Exception;
+
+class Queue
+{
+  public function __construct(private array $middle, private array $vars, private Closure $controller)
+  {
+  }
+
+  public function next(Request $request)
+  {
+    if (empty($this->middle))
+      return call_user_func_array($this->controller, $this->vars);
+
+    foreach ($this->middle as $key => $middleware) {
+      if (!is_callable($middleware))
+        throw new Exception("Middleware inválido", 500);
+
+
+      $actualMiddleware = new $middleware;
+      unset($this->middle[$key]);
+
+      $queue = $this;
+      $next = function () use ($queue, $request) {
+        return $queue->next($request);
+      };
+
+      $actualMiddleware($request, $next);
+    }
+  }
+}
