2915620376dbd0c72d675fef35def3b9da05b6ec
Gabriel Vitor <git624886@gmail.com>
2023-02-16 15:53:51 -0300

    feat: include .env

--- public/index.php
+++ public/index.php
@@ -5,5 +5,5 @@ use App\Http\Response;
 use App\Http\Router;
 
 require_once __DIR__ . '/../vendor/autoload.php';
+include_once __DIR__ . '/../config/env.php';
 include_once __DIR__ . '/../config/init.php';
-
