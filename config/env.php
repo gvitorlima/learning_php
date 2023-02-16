05c9fabcecc1f2e61add40da21d54e6d22420d6f
Gabriel Vitor <git624886@gmail.com>
2023-02-16 15:53:41 -0300

    feat: dados .env

---
+++ config/env.php
@@ -0,0 +1,12 @@
+<?php
+
+$path = __DIR__ . '/../.env';
+
+$file = file($path, FILE_SKIP_EMPTY_LINES);
+foreach ($file as $line) {
+  if (str_starts_with($line, '#')) {
+    continue;
+  }
+
+  putenv($line);
+}
