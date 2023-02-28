39c42814a9fde167df72051e15c376b960dd259d
Gabriel Vitor <git624886@gmail.com>
2023-02-28 11:04:00 -0300

    feat: inicio jwt

--- app/Http/Middlewares/Jwt.php
+++ app/Http/Middlewares/Jwt.php
@@ -10,15 +10,19 @@ class Jwt extends AbstractMiddleware
 {
   public function handle(Request $request, Closure $next)
   {
-    $this->verifyJwt();
+    $this->newJwt($request);
     return $next($request);
   }
 
-  private function verifyJwt()
+  private function verifyJwt(Request $request)
   {
     try {
+      $token = $request->getHeaders()['Authorization'] ?? throw new Exception("Jwt autenticação necessária", 400);
+      if (!isset($token))
+        throw new Exception("Verifique a autenticação passada e tente novamente", 400);
+
       echo '<pre>';
-      print_r('oi');
+      print_r($_SERVER);
       echo '</pre>';
       exit;
     } catch (Exception $err) {
@@ -26,6 +30,18 @@ class Jwt extends AbstractMiddleware
       print_r($err->getMessage());
       echo '</pre>';
       exit;
+    }
+  }
+
+  public static function newJwt()
+  {
+    try {
+
+      $header = [
+        'alg' => 'HS256',
+        'typ' => 'JWT'
+      ];
+    } catch (Exception $err) {
     }
   }
 }
