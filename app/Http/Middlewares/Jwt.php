b261ea37de77236fd9b49690454df7431a154abf
Gabriel Vitor <git624886@gmail.com>
2023-03-01 11:06:01 -0300

    fix: base64 inválido

--- app/Http/Middlewares/Jwt.php
+++ app/Http/Middlewares/Jwt.php
@@ -49,11 +49,10 @@ class Jwt extends AbstractMiddleware
       $header   = json_encode($header);
       $payload  = json_encode($payload);
 
-      $base64data = base64_encode($header . '.' . $payload);
+      $base64data = self::base64encode($header) . '.' . self::base64encode($payload);
 
-      $jwt = hash_hmac('sha512', $base64data, getenv('JWT_SALT'));
-      $jwt = base64_encode($header) . '.' . base64_encode($payload) . '.' . base64_encode($jwt);
-      $jwt =  str_replace('=', '', $jwt);
+      $jwt = hash_hmac('sha256', $base64data, getenv('JWT_SALT'), true);
+      $jwt = self::base64encode($header) . '.' . self::base64encode($payload) . '.' . self::base64encode($jwt);
 
       return [
         'token' => $jwt
@@ -64,5 +63,27 @@ class Jwt extends AbstractMiddleware
       echo '</pre>';
       exit;
     }
+  }
+
+  /**
+   * Substitui alguns caracteres do base64 gerado.
+   * 
+   * Explicação:
+   * 
+   * Para tornar o token JWT seguro, os caracteres '+' e '/' são substituídos por outros caracteres que não são comumente usados no base64. 
+   * Essa técnica é chamada de URL-safe base64 encoding. A substituição do caractere '+' pelo caractere '-' e do caractere '/' pelo caractere '_'
+   * evita que esses caracteres sejam interpretados de maneira incorreta pelos navegadores da Web e outros softwares que manipulam URLs.
+   * 
+   * O caractere '=' é usado para preencher o final da sequência de caracteres codificados em base64 para que ela tenha um número inteiro de blocos
+   * de 4 caracteres. Isso é importante porque os algoritmos de decodificação de base64 esperam que a sequência de caracteres tenha um 
+   * número inteiro de blocos de 4 caracteres. No entanto, a presença do caractere = no final da sequência pode ser 
+   * interpretada incorretamente pelos softwares que manipulam URLs, especialmente em ambientes web.
+   * 
+   * Por isso, o caractere '=' é omitido no final do token JWT, a menos que seja estritamente necessário para preencher o último bloco de caracteres.
+   * Isso garante a compatibilidade do token JWT em ambientes web e evita possíveis problemas de segurança.
+   */
+  private static function base64encode(string $data)
+  {
+    return str_replace(['+', '/', '='], ['-', '_', ''], base64_encode($data));
   }
 }
