dd538c63cb364f0143c81836ce77f0a61bccbbf8
Gabriel Vitor <git624886@gmail.com>
2023-03-16 14:02:35 -0300

    feat/fix: clear alias sendo usado no querybuilder

--- app/Utils/QueryBuilderUtils.php
+++ app/Utils/QueryBuilderUtils.php
@@ -7,8 +7,14 @@ class QueryBuilderUtils
   public static function clearString(string $field): string
   {
     $field = htmlspecialchars($field);
-    $field = preg_replace("/((?:\W)(?<!-))/", '', $field);
+    $field = preg_replace("--", '', $field);
 
     return $field;
+  }
+
+  public static function clearAlias(string $stringWithAlias): string
+  {
+    $string = str_replace('.', '', $stringWithAlias);
+    return $string;
   }
 }
