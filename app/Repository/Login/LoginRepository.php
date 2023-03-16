9e2d33c3129d4d9dba121ef1c76c9d13c96b6f01
Gabriel Vitor <git624886@gmail.com>
2023-03-16 15:29:29 -0300

    fix: query builder antigo ainda em uso

--- app/Repository/Login/LoginRepository.php
+++ app/Repository/Login/LoginRepository.php
@@ -2,22 +2,26 @@
 
 namespace App\Repository\Login;
 
-use App\Databases\Database;
-use App\Databases\enumPrefix;
-use App\Databases\LearningAuth;
+use App\Database\Configs\Database;
+use App\Database\Configs\Query;
+use App\Database\LearningAuth;
 
 class LoginRepository
 {
   private Database $database;
+  private Query $queryBuilder;
   public function __construct()
   {
     $this->database = new Database(LearningAuth::getInstance());
+    $this->queryBuilder = Query::getInstance();
   }
 
   public function get(string $email)
   {
-    $sql = "SELECT * FROM AUTH a WHERE a.EMAIL = '$email'";
-    $data = $this->database->executeQuery($sql);
+    $query = $this->queryBuilder->select()
+      ->from('AUTH a')->where('a.EMAIL', $email)->run();
+
+    $data = $this->database->executeQuery($query);
 
     return $data[array_key_first($data)] ?? [];
   }
