a6b6523f76be854a5e3c63ff45277c26add3aebf
Gabriel Vitor <git624886@gmail.com>
2023-03-16 15:59:14 -0300

    feat: funções para padronizar retorno

---
+++ config/utils.php
@@ -0,0 +1,23 @@
+<?php
+
+function formatResponse(int $code, mixed $content = null, bool $json = false): string|array
+{
+  $return = [
+    'code' => $code,
+    'content' => $content
+  ];
+
+  if (!$json) {
+    return $return;
+  }
+
+  return json_encode($return);
+}
+
+function formatResponseError(Throwable $error): array
+{
+  return [
+    'code' => $error->getCode(),
+    'message' => $error->getMessage()
+  ];
+}
