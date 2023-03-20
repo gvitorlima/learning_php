10c9fd93689874245d0e023553765f521be59054
Gabriel Vitor <git624886@gmail.com>
2023-03-20 11:41:10 -0300

    fix: include de utils não era feito no index

--- public/index.php
+++ public/index.php
@@ -6,4 +6,7 @@ use App\Http\Router;
 
 require_once __DIR__ . '/../vendor/autoload.php';
 include_once __DIR__ . '/../config/env.php';
+include_once __DIR__ . '/../config/utils.php';
+
+// ! INCLUA OS TREM ANTES DE INICIAR OS NEGOCIO
 include_once __DIR__ . '/../config/init.php';
