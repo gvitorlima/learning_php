9e062f74cc6f9a878cff7f9fbce5863bb7853d65
Gabriel Vitor <git624886@gmail.com>
2023-03-20 13:38:31 -0300

    fix: queuerules não posta  pack de commit anterior

---
+++ app/Http/Rules/QueueRules.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Rules;
+
+use App\Http\Request;
+use Exception;
+
+class QueueRules
+{
+  public function __construct(private array $rules)
+  {
+  }
+
+  public function next(Request $request)
+  {
+    if (empty($this->rules))
+      return;
+    $firstMiddleware = array_key_first($this->rules);
+    $middleware = $this->rules[$firstMiddleware];
+
+    if (!class_exists($middleware))
+      throw new Exception("Regra inválida", 500);
+
+    unset($this->rules[$firstMiddleware]);
+    $actualMiddleware = new $middleware;
+
+    $queue = $this;
+    $next = function () use ($queue, $request) {
+      return $queue->next($request);
+    };
+
+    return $actualMiddleware($request, $next);
+  }
+}
